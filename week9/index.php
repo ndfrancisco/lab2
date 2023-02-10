c0b7638f4d250c4e4f78b547d40eefbc6be97335
ndfrancisco <71538611+ndfrancisco@users.noreply.github.com>
2023-02-09 16:06:49 -0800

    Update index.php

--- week9/index.php
+++ week9/index.php
@@ -133,15 +133,65 @@ img {
 <img src="tmp_1671153587521.jpg">
 <p>Turn on your speakers to hear the xylophone</p>
 <table>
-    <tr>
+	<tr>
 	<td><button class="btn btn1" onclick="sound1()">DO</button></td>
-    <td><button class="btn btn2" onclick="sound2()">RE</button></td>
-    <td><button class="btn btn3" onclick="sound3()">MI</button></td>
-    <td><button class="btn btn4" onclick="sound4()">FA</button></td>
+    	<td><button class="btn btn2" onclick="sound2()">RE</button></td>
+    	<td><button class="btn btn3" onclick="sound3()">MI</button></td>
+    	<td><button class="btn btn4" onclick="sound4()">FA</button></td>
 	<td><button class="btn btn5" onclick="sound5()">SO</button></td>
 	<td><button class="btn btn6" onclick="sound6()">LA</button></td>
 	<td><button class="btn btn7" onclick="sound7()">TI</button></td>
 	</tr>
 </table>
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 </body>
 </html>
